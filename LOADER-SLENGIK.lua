f87f7336d7b5edbc19a31515f17f8d6468dfb7b5
DRAKE06-dell <d18969757@gmail.com>
2026-01-13 14:39:08 -0300

    Delete LOADER-SLENGIK.lua

--- LOADER-SLENGIK.lua
+++
@@ -1,9 +0,0 @@
-print("SLENGIK Loader iniciado")
-
-game.StarterGui:SetCore("SendNotification", {
-    Title = "SLENGIK",
-    Text = "Loader carregado com sucesso!",
-    Duration = 5
-})
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DRAKE06-dell/-/main/Main-SLENGIK.lua"))()
